d0d53016b8a1657dfbe24a8e2d5b44822a3e8d6f
Gabecx <jessicagabica54@gmail.com>
2026-04-27 20:12:45 +0800

    Initial commit

---
+++ records/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrFaculty(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.groups.filter(
+            name__in=['Admin', 'Faculty']
+        ).exists()
